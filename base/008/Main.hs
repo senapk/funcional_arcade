c1585d0fb480428714a44f3da76d098d11524c66
David Sena <sena.ufc@gmail.com>
2020-02-29 20:56:23 -0300

    adding 008 alternative solution

--- base/008/Main.hs
+++ base/008/Main.hs
@@ -1,8 +1,14 @@
-sublist begin' end' xs = let
-    size  = length xs
-    begin = if begin' < 0 then begin' + size else begin'
-    end   = if end' < 0   then end' + size   else end'
-    in drop begin . take end $ xs
+--sublist begin' end' xs = let
+--    size  = length xs
+--    begin = if begin' < 0 then begin' + size else begin'
+--    end   = if end' < 0   then end' + size   else end'
+--    in drop begin . take end $ xs
+
+
+sublist begin' end' xs = drop begin . take end $ xs
+    where size  = length xs
+          begin = if begin' < 0 then begin' + size else begin'
+          end   = if end' < 0   then end' + size   else end'
 
 main = do
     begin <- readLn :: IO Int
